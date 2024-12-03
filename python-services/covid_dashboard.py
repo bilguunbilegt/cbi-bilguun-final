e24c027aa45d9b0ba45a27789d24ffa01a6496fd
bilguunbilegt <bilguunbyambadorj2026@u.northwestern.edu>
2024-12-02 20:19:51 -0600

    Update covid_dashboard.py

--- python-services/covid_dashboard.py
+++ python-services/covid_dashboard.py
@@ -47,4 +47,5 @@ app.layout = html.Div([
 ])
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    # Explicitly specify the port as 8050
+    app.run_server(debug=True, host="0.0.0.0", port=8050)
